d71a5751f6c2ce99fd430d54cd9ee0dd9fc10de0
joseph3559 <scottjoe3559@gmail.com>
2022-10-09 23:02:16 +0300

    Update migration for Create restaurants

--- db/migrate/20221009172746_create_restaurants.rb
+++ db/migrate/20221009172746_create_restaurants.rb
@@ -1,6 +1,9 @@
-class CreateRestaurants < ActiveRecord::Migration[7.0]
+class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
-    create_table :restaurants do |t|
+    create_table :restaurant_pizzas do |t|
+      t.integer :price
+      t.integer :pizza_id, null: false
+      t.integer :restaurant_id, null: false
 
       t.timestamps
     end
