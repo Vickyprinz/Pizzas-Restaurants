235c29981f1adf5b435fcce5e5c7807819c94531
joseph3559 <scottjoe3559@gmail.com>
2022-10-09 21:06:34 +0300

    Define controller for restaurants controller with crud operation and error handler

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,51 +1,30 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  # GET /restaurants
   def index
-    @restaurants = Restaurant.all
-
-    render json: @restaurants
+    restaurants = Restaurant.all
+    render json: restaurants, status: :ok
   end
 
-  # GET /restaurants/1
   def show
-    render json: @restaurant
+    restaurant = find_restaurant
+    render json: restaurant, serializer: RestaurantPizzaSerializer, status: :ok
   end
 
-  # POST /restaurants
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-
-    if @restaurant.save
-      render json: @restaurant, status: :created, location: @restaurant
-    else
-      render json: @restaurant.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /restaurants/1
-  def update
-    if @restaurant.update(restaurant_params)
-      render json: @restaurant
-    else
-      render json: @restaurant.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /restaurants/1
   def destroy
-    @restaurant.destroy
+    restaurant = find_restaurant
+    restaurant.destroy
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.fetch(:restaurant, {})
-    end
+  def find_restaurant
+    Restaurant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Restaurant not found"}, status: :not_found
+  end
+
 end
