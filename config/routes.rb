9ff172749e6a7545ae26ec8368197992ca3eebd9
joseph3559 <scottjoe3559@gmail.com>
2022-10-09 20:58:44 +0300

    Generate Restaurant controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :restaurants
   resources :restaurant_pizzas
   resources :pizzas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
