380f7d7383d9a7325df825c83808b158f4f260d3
joseph3559 <scottjoe3559@gmail.com>
2022-10-09 21:05:02 +0300

    Define controller for Restaurant_pizzas Controller with rescue, create and  params

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,51 +1,21 @@
 class RestaurantPizzasController < ApplicationController
-  before_action :set_restaurant_pizza, only: %i[ show update destroy ]
 
-  # GET /restaurant_pizzas
-  def index
-    @restaurant_pizzas = RestaurantPizza.all
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-    render json: @restaurant_pizzas
-  end
-
-  # GET /restaurant_pizzas/1
-  def show
-    render json: @restaurant_pizza
-  end
-
-  # POST /restaurant_pizzas
   def create
-    @restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
-
-    if @restaurant_pizza.save
-      render json: @restaurant_pizza, status: :created, location: @restaurant_pizza
-    else
-      render json: @restaurant_pizza.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /restaurant_pizzas/1
-  def update
-    if @restaurant_pizza.update(restaurant_pizza_params)
-      render json: @restaurant_pizza
-    else
-      render json: @restaurant_pizza.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /restaurant_pizzas/1
-  def destroy
-    @restaurant_pizza.destroy
+    restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params)
+    pizza = Pizza.find(restaurant_pizza.pizza_id)
+    render json: pizza, status: :created
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant_pizza
-      @restaurant_pizza = RestaurantPizza.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def restaurant_pizza_params
-      params.fetch(:restaurant_pizza, {})
-    end
+  def restaurant_pizza_params
+    params.permit(:price, :pizza_id, :restaurant_id)
+  end
+  
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
